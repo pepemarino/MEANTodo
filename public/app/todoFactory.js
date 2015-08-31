bc38d1509e8ce00c3af1a51a8844b3890791dd4c
Jose Marino <jrmarino@telus.net>
2015-08-30 21:42:42 -0700

    socketFactory does not emit Todo update currently

--- public/app/todoFactory.js
+++ public/app/todoFactory.js
@@ -45,16 +45,6 @@
 						callback.apply(socket, args);
 					});
 				});
-			},
-			emit: function (eventName, data, callback) {
-				socket.emit(eventName, data, function () {
-					var args = arguments;
-					$rootScope.$apply(function () {
-					if (callback) {
-						callback.apply(socket, args);
-					}
-					});
-				});
 			}
 		};
 	}
